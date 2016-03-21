63ab76b892be6985c90462b883390c0295197ebf
Alex Faber <alex.faber@singlewire.com>
2016-03-20 22:24:13 -0500

    implement user authentication route

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var config = require('../config');
 
 router.post('/signup', function(req, res, next){
     req.checkBody('firstName', 'firstName is required and needs to be Alphanumeric').isAlphanumeric();
@@ -34,6 +36,45 @@ router.post('/signup', function(req, res, next){
             res.json({message: "Success"});
         }
     });
+});
+
+router.post('/token', function(req, res, next){
+    req.checkBody('email', 'email is required and must be valid').isEmail();
+    req.checkBody('password', 'password is required and needs to be Alphanumeric').isAlphanumeric();
+    var errors = req.validationErrors();
+    if(errors){
+        res.status(400).json({message: "There were validation errors", errors: errors});
+        return
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            res.status(500).json({
+                success: false,
+                message: "Error when checking for user with email: " + req.body.email,
+                error: err
+            });
+            return
+        }
+        if(!user){
+            res.status(404).json({
+                success: false,
+                message: "Unable to find user with email: " + req.body.email
+            });
+            return
+        }
+        if(user.password != req.body.password){
+            res.status(401).json({
+                success: false,
+                message: "Passwords do not match"
+            });
+            return
+        }
+        jwt.sign(user, config.secret,{expiresInMinutes: 1440}, function(token){
+            res.json({success: true, token: token});
+        });
+
+    })
 });
 
 module.exports = router;
