c10dbca60c7fd6a8dcd9cee875141aeacab81445
Alex Faber <alexfaber2011@gmail.com>
2016-03-28 15:35:32 -0500

    add production environment mongo connection

--- mongoose.js
+++ mongoose.js
@@ -2,7 +2,14 @@
  * Created by alexfaber on 3/11/16.
  */
 var mongoose = require('mongoose');
+var config = require('./config');
 
-mongoose.connect('mongodb://localhost/keeper-api');
+if (process.env.NODE_ENV == 'development') {
+    mongoose.connect('mongodb://localhost/keeper-api');
+} else {
+    var uri = 'mongodb://ds025439.mlab.com:25439/keeper-api';
+    var options = {user: config.db.user, pass: config.db.password};
+    mongoose.connect(uri, options);
+}
 
 module.exports = mongoose;
