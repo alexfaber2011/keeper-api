2dd8db327c9cccf3cbdd822f08e03d613142f7ea
Alex Faber <alexfaber2011@gmail.com>
2016-03-30 22:08:10 -0500

    bcrypt passwords when storing and comparing

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -6,40 +6,47 @@ var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var config = require('../config');
+var password = require('../utilities/password');
 
 router.post('/signup', function(req, res, next){
     req.checkBody('firstName', 'firstName is required and needs to be Alphanumeric').isAlphanumeric();
     req.checkBody('lastName', 'lastName is required and needs to be Alphanumeric').isAlphanumeric();
     req.checkBody('email', 'email is required and must be valid').isEmail();
-    req.checkBody('password', 'password is required and needs to be Alphanumeric').isAlphanumeric();
+    req.checkBody('password', 'password is required, needs to be 6-70 characters long, and needs to be Alphanumeric').isLength({min: 6, max: 70}).isAscii();
     var errors = req.validationErrors();
     if(errors){
         res.status(400).json({message: "There were validation errors", errors: errors});
         return
     }
 
-    var user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    });
+    password.hash(req.body.password)
+        .then(function(hash){
+            var user = new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: hash
+            });
 
-    user.save(function(err){
-        if(err && err.code == 11000) {
-            res.status(400).json({message: "Email is not unique", error: err})
-        } else if(err){
-            res.status(500).json({message: "Unable to create user", error: err});
-        } else {
-            console.log("cooL");
-            res.json({message: "Success"});
-        }
-    });
+            user.save(function(err){
+                if(err && err.code == 11000) {
+                    res.status(400).json({message: "Email is not unique", error: err})
+                } else if(err){
+                    res.status(500).json({message: "Unable to create user", error: err});
+                } else {
+                    console.log("cooL");
+                    res.json({message: "Success"});
+                }
+            });
+        })
+        .catch(function(err){
+            res.status(500).json({message: "Error when encrypting password", error: err})
+        });
 });
 
 router.post('/token', function(req, res, next){
     req.checkBody('email', 'email is required and must be valid').isEmail();
-    req.checkBody('password', 'password is required and needs to be Alphanumeric').isAlphanumeric();
+    req.checkBody('password', 'password is required, needs to be 6-70 characters long, and needs to be Alphanumeric').isLength(6, 70).isAscii();
     var errors = req.validationErrors();
     if(errors){
         res.status(400).json({message: "There were validation errors", errors: errors});
@@ -62,18 +69,19 @@ router.post('/token', function(req, res, next){
             });
             return
         }
-        if(user.password != req.body.password){
-            res.status(401).json({
-                success: false,
-                message: "Passwords do not match"
+        password.compare(req.body.password, user.password)
+            .then(function(){
+                jwt.sign(user, config.secret,{expiresIn: 86400}, function(token){
+                    res.json({success: true, token: token});
+                });
+            })
+            .catch(function(){
+                res.status(401).json({
+                    success: false,
+                    message: "Passwords do not match"
+                });
             });
-            return
-        }
-        jwt.sign(user, config.secret,{expiresIn: 86400}, function(token){
-            res.json({success: true, token: token});
-        });
-
-    })
+    });
 });
 
 module.exports = router;
