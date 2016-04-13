e33858493e1481c8611ba5f115daacd296585294
Alex Faber <alexfaber2011@gmail.com>
2016-04-13 15:31:24 -0500

    create docker file that uses https

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
  * Created by alexfaber on 3/16/16.
  */
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var app = express();
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var morgan = require('morgan');
-var winston = require('winston'),
-    Papertrail = require('winston-papertrail').Papertrail;
+var winston = require('winston');
+var Papertrail = require('winston-papertrail').Papertrail;
 
 
 var authentication = require('./middlewares/authentication');
@@ -25,7 +28,7 @@ var logger = new winston.Logger({
 });
 
 logger.stream = {
-    write: function(message, encoding){
+    write: function (message, encoding) {
         logger.info(message);
     }
 };
@@ -34,14 +37,31 @@ logger.stream = {
 //Parse when application/json
 app.use(bodyParser.json());
 app.use(expressValidator(expressValidatorOptions));
-app.use(morgan('dev', { "stream": logger.stream }));
+app.use(morgan('dev', {"stream": logger.stream}));
 
 //Add all controllers to our app with some authentication middleware
 app.use(authentication.verify);
 app.use(require('./controllers'));
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000...')
-});
+
+if (process.env.NODE_ENV == 'development') {
+    //If we're working on a development machine, start an express server that listens on port 3000
+    app.listen(3000, function () {
+        console.log('Listening on port 3000...');
+    });
+} else {
+    var credentials = {
+        key: fs.readFileSync('clotho.cloud/privkey.pem'),
+        cert: fs.readFileSync('clotho.cloud/fullchain.pem'),
+        ca: fs.readFileSync('clotho.cloud/chain.pem')
+    };
+    var httpServer = http.createServer(app);
+    var httpsServer = https.createServer(credentials, app);
+
+    httpServer.listen(80);
+    httpsServer.listen(443);
+    console.log("HTTP: listening on port 80");
+    console.log("HTTPS: listening on port 443");
+}
 
 module.exports = app;
